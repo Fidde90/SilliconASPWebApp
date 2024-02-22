48fa95e316683044ed64e07d615da822d7fae7fd
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-02-22 19:08:47 +0100

    Base structure and cleanup done.

--- SilliconASPWebApp/Controllers/HomeController.cs
+++ SilliconASPWebApp/Controllers/HomeController.cs
@@ -1,32 +1,14 @@
-using Microsoft.AspNetCore.Mvc;
-using SilliconASPWebApp.Models;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SilliconASPWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
+            ViewData["Title"] = "Home";
 
-        public IActionResult Privacy()
-        {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
