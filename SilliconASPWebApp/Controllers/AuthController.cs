437d4f93f8aba5bf70153b5e72cd5a8056b6158b
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-02-23 22:13:53 +0100

    Added Title

--- SilliconASPWebApp/Controllers/AuthController.cs
+++ SilliconASPWebApp/Controllers/AuthController.cs
@@ -4,7 +4,7 @@ namespace SilliconASPWebApp.Controllers
 {
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        public IActionResult SignUp()
         {
             return View();
         }
