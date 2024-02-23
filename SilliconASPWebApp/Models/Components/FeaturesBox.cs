b235064d618632f3b38b271c2c5b76d6e75916d5
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-02-23 20:23:21 +0100

    Features section done, deleted viewmodels, going for models

---
+++ SilliconASPWebApp/Models/Components/FeaturesBox.cs
@@ -0,0 +1,10 @@
+﻿namespace SilliconASPWebApp.Models.Components
+{
+    public class FeaturesBox
+    {
+        public string BoxClass { get; set; } = null!;
+        public ImageModel Image { get; set; } = null!;
+        public string BoxTitle { get; set; } = null!;
+        public string BoxText { get; set; } = null!;
+    }
+}
