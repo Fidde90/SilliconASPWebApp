969a8468bf4e5dd5a2bead7e3f2738b52a9d75cd
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-02-24 00:05:13 +0100

    SignUp form in progress, changed the heigth value in scss to 100%, form model created

--- SilliconASPWebApp/Controllers/AuthController.cs
+++ SilliconASPWebApp/Controllers/AuthController.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SilliconASPWebApp.ViewModels.Views;
 
 namespace SilliconASPWebApp.Controllers
 {
     public class AuthController : Controller
     {
         [Route("/signup")]
+        [HttpGet]
         public IActionResult SignUp()
         {
-            return View();
+            var viewModel = new SignUpViewModel();
+            return View(viewModel);
+        }
+
+        [Route("/signup")]
+        [HttpPost]
+        public IActionResult SignUp(SignUpViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+                return View(viewModel);
+
+            return RedirectToAction("SignIn", "Auth");
         }
     }
 }
---
+++ SilliconASPWebApp/ViewModels/Views/SignUpViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SilliconASPWebApp.Models.Sections;
+
+namespace SilliconASPWebApp.ViewModels.Views
+{
+    public class SignUpViewModel
+    {
+        public string Title { get; set; } = "Sign up";
+
+        public SignUpFormModel From { get; set; } = new SignUpFormModel();
+
+
+    }
+}
