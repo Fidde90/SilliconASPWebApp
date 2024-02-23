7ed5459e72f34bdd824360591d8fb1bfdf7c817a
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-02-23 22:50:46 +0100

    Changed the route name to /signup and added Scss file

--- SilliconASPWebApp/Controllers/AuthController.cs
+++ SilliconASPWebApp/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ namespace SilliconASPWebApp.Controllers
 {
     public class AuthController : Controller
     {
+        [Route("/signup")]
         public IActionResult SignUp()
         {
             return View();
