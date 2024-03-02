d6026c167b752585295ccac0bc5fe43cebd35871
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-03-02 18:50:40 +0100

    database and connectionstring fixed

--- SilliconASPWebApp/Program.cs
+++ SilliconASPWebApp/Program.cs
@@ -1,7 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
 
@@ -13,6 +19,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
+//app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
