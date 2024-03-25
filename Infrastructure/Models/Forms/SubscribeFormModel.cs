88da13bea0d4aae3d253cfc247a9e218a7a1c60e
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-03-25 14:11:42 +0100

    edited scss for the subscribe form and added a form model.

--- Infrastructure/Models/Forms/SubscribeFormModel.cs
+++ Infrastructure/Models/Forms/SubscribeFormModel.cs
@@ -4,7 +4,7 @@ namespace Infrastructure.Models.Forms
 {
     public class SubscribeFormModel
     {
-        [Required(ErrorMessage = "Email is required")]
+        [Required(ErrorMessage = "Enter a vaild email")]
         [DataType(DataType.EmailAddress)]
         [Display(Prompt = "Your Email")]
         [RegularExpression("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email")]
